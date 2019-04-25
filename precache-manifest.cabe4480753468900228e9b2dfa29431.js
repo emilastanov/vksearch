3780949c2036269a9d192d201b81db5ef876dfd1
Emil Astanov <32235977+emilastanov@users.noreply.github.com>
2019-04-25 16:50:57 +0300

    Add files via upload

---
+++ precache-manifest.cabe4480753468900228e9b2dfa29431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56a673b90ba86b35da38",
+    "url": "/static/js/main.5732ce7e.chunk.js"
+  },
+  {
+    "revision": "10ac0c2bd84be7090291",
+    "url": "/static/js/2.d3fbd0cf.chunk.js"
+  },
+  {
+    "revision": "10ac0c2bd84be7090291",
+    "url": "/static/css/2.08bb22ea.chunk.css"
+  },
+  {
+    "revision": "9fc3893eb01de2c8c2fd061058c30c72",
+    "url": "/index.html"
+  }
+];
